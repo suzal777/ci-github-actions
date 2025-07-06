7d94dff7f97ddef891d579569f2e9843f2404611
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-07-06 10:06:42 +0545

    fix: tslint backend1

--- code/backend1/src/index.ts
+++ code/backend1/src/index.ts
@@ -34,5 +34,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+ // console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
