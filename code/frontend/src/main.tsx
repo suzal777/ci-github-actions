1873bebc9ee2cb8520d083cbc00bdb6cb06d49c1
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-07-07 20:43:40 +0545

    feat: updated versioning in workflow

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
