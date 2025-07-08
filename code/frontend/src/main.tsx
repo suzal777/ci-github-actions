542db9cf1f00d0ce15e5f7f109b15baef2a96121
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-07-08 18:50:33 +0545

    fix: updated src code

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
