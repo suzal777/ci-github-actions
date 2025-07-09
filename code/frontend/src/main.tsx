fd570da730d76b098ca5b145b642a7f2c17f7376
Sujal Phaiju <sujalphaiju777@gmail.com>
2025-07-09 19:59:39 +0545

    fix: update github pages deploy

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -8,4 +8,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+// testing from dev againnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
